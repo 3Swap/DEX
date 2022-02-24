20d9eb86e3da27a38968f9a9fb6a71e10c50b8a4
Constantino <Okonemmanuelmma@gmail.com>
2022-02-24 19:02:32 +0100

    button and errm

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 
 type Props = {};
 
+const ButtonDiv = styled.button`
+  width: 145px;
+  height: 38px;
+  background: rgba(69, 0, 160, 1);
+  border-radius: 5px;
+  color: white;
+`;
+
 const Button = ({ name, onClick }: any) => {
-  const ButtonDiv = styled.button`
-    width: 145px;
-    height: 38px;
-    background: rgba(69, 0, 160, 1);
-    border-radius: 5px;
-    color: white;
-  `;
   return (
     <div>
       <ButtonDiv>Button</ButtonDiv>
