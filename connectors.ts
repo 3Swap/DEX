e2de50ff0885c4f297595606c5e018a5a4e30237
Kingsley Victor <javaprodigy56@gmail.com>
2022-02-25 01:13:20 +0100

    create connectors and hooks

--- connectors.ts
+++ connectors.ts
@@ -1,4 +1,12 @@
 import { ChainId } from '3swap-sdk';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
-export const injected = new InjectedConnector({ supportedChainIds: Object.keys(ChainId).map(chain => Number(chain)) });
+export const injected = new InjectedConnector({
+  supportedChainIds: [
+    ChainId.AVALANCHE_TESTNET,
+    ChainId.BINANCE_TESTNET,
+    ChainId.FANTOM_TESTNET,
+    ChainId.MATIC_TESTNET,
+    ChainId.ROPSTEN
+  ]
+});
