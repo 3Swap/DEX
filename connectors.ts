1d8788f20500a1f0c933c0597a725ac415bd0b81
Kingsley Victor <javaprodigy56@gmail.com>
2022-02-25 11:13:46 +0100

    update connectors

--- connectors.ts
+++ connectors.ts
@@ -1,12 +1,23 @@
-import { ChainId } from '3swap-sdk';
+import { ChainId, URLS } from '3swap-sdk';
 import { InjectedConnector } from '@web3-react/injected-connector';
+import { NetworkConnector } from '@web3-react/network-connector';
 
 export const injected = new InjectedConnector({
   supportedChainIds: (() => {
     let chainIds: Array<number> = [];
-    for (const key in ChainId) {
-      chainIds = [...chainIds, Number(ChainId[key])];
-    }
+
+    for (const key in ChainId) chainIds = [...chainIds, Number(ChainId[key])];
+
     return chainIds;
+  })()
+});
+
+export const network = new NetworkConnector({
+  urls: (() => {
+    let links: { [chainId: number]: string } = {};
+
+    for (const key in ChainId) links = { ...links, [Number(ChainId[key])]: URLS[Number(ChainId[key])] };
+
+    return links;
   })()
 });
