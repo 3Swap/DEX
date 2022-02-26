887ddb256fe9066683f1b48fc7f8e57dabb13e65
Kingsley Victor <javaprodigy56@gmail.com>
2022-02-26 21:43:53 +0100

    new commit

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-type Props = {};
+type CustomButtonProps = {};
 
-const ButtonDiv = styled.button`
+const CustomButton = styled.button`
   width: 145px;
   height: 38px;
   background: rgba(69, 0, 160, 1);
@@ -14,7 +14,7 @@ const ButtonDiv = styled.button`
 const Button = ({ name, onClick }: any) => {
   return (
     <div>
-      <ButtonDiv>Button</ButtonDiv>
+      <CustomButton>Button</CustomButton>
     </div>
   );
 };
